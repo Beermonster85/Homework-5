9e14026e3d4752be4a7053efb104f77121e6e22f
Kirill <kiril-shitikov@yandex.ru>
2023-04-26 15:54:29 +0300

    docs: HW task #1

--- Program.cs
+++ Program.cs
@@ -83,3 +83,26 @@
 // }
 // Console.WriteLine(String.Join(",", numArr));
 // Console.WriteLine("Кол-во двухзначных чисел = " + num);
+
+
+// ДЗ
+// Задача №1
+// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+// int[] numArr = new int[4];
+
+// int count = 0;
+
+// for (int i = 0; i < numArr.Length; i++)
+// {
+//     numArr[i] = new Random().Next(100, 999);
+
+//     if (numArr[i] % 2 == 0)
+//     {
+//         count++;
+//     }
+
+// }
+// Console.WriteLine(String.Join(",", numArr));
+// Console.WriteLine(count);
